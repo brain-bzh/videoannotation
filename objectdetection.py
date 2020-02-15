7d30d36531741c79de744262b6084f33ae58068f
Nicolas Farrugia <nicolas.farrugia@imt-atlantique.fr>
2020-02-15 23:28:20 +0100

    first version

---
+++ objectdetection.py
@@ -0,0 +1,131 @@
+from torchvision.models.detection import fasterrcnn_resnet50_fpn
+import torch
+from torchvision.io import read_video,read_video_timestamps
+
+import matplotlib.patches as patches
+from matplotlib import pyplot as plt
+
+import datetime
+
+
+COCO_INSTANCE_CATEGORY_NAMES = [
+    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+]
+
+#### imagenet categories
+
+def cat_file():
+    # load classes file
+    categories = []
+    try:
+        f = open('categories.txt', 'r')
+        for line in f:
+            cat = line.split(',')[0].split('\n')[0]
+            if cat != 'classes':
+                categories.append(cat)
+        f.close()
+        print('Number of categories:', len(categories))
+    except:
+        print('Error opening file ' + ' categories.txt')
+        quit()
+    return categories
+
+
+categories = cat_file() # load category file
+
+
+
+
+def annotate_img(preds,vframes,n_obj=5):
+
+    ### vframes  : last three dims input tensor to faster_rcnn
+
+    ### preds : dictionary of outputs of fater_rcnn
+
+    global COCO_INSTANCE_CATEGORY_NAMES
+
+    predlabels = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in preds['labels'].numpy()]
+    scores = [i for i in preds['scores'].detach().numpy()]
+    bboxes = [i for i in preds['boxes'].detach().numpy()]
+
+
+    test_im = vframes.permute(1,2,0).numpy()
+
+    # Create figure and axes
+    fig,ax = plt.subplots(1,figsize=(20,25))
+
+    # Display the image
+    ax.imshow(test_im)
+
+
+    #### add the annotations 
+
+    for curbbox,curlab in zip(bboxes[:n_obj],predlabels[:n_obj]):
+
+        topleftx = curbbox[0]
+        toplefty = curbbox[1]
+
+        bottomrightx = curbbox[2]
+        bottomrighty = curbbox[3]
+
+
+        # Create a Rectangle patch
+        rect = patches.Rectangle((topleftx,toplefty),abs(bottomrightx-topleftx),abs(bottomrighty-toplefty),linewidth=1,edgecolor='r',facecolor='none')
+
+        # Add the patch to the Axes
+        ax.add_patch(rect)
+
+        ax.text(topleftx,toplefty,curlab,c='white',fontsize=16)
+    plt.show()
+    return fig
+
+
+def gen_srt(strlabel,onset,srtfile,duration=2,num=1):
+
+    starttime = onset
+    endtime = starttime + duration
+
+    string_start = datetime.time(0,starttime//60,starttime%60).strftime("%H:%M:%S")
+
+    string_end = datetime.time(0,endtime//60,endtime%60).strftime("%H:%M:%S")
+
+    with open(srtfile,'a') as f:
+        f.write("{}\n".format(num+1))
+        f.write("{starttime} --> {endtime}\n".format(starttime=string_start,endtime=string_end))
+        f.write("{}\n".format(strlabel))
+        f.write("\n")
+
+def gen_srt_coco_multiple(allpreds,onsets,srtfile,n_obj=5):
+    global COCO_INSTANCE_CATEGORY_NAMES
+
+    ## check that both lists have the same size 
+    if len(allpreds) != len(onsets):
+        raise(ValueError('List of predictions and onsets have different sizes'))
+
+    for num,(curpred,curonset) in enumerate(zip(allpreds,onsets)):
+
+        predlabels = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in curpred['labels'].numpy()[:n_obj]]
+
+        starttime = curonset
+        endtime = curonset + 2
+
+        string_start = datetime.time(0,starttime//60,starttime%60).strftime("%H:%M:%S")
+
+        string_end = datetime.time(0,endtime//60,endtime%60).strftime("%H:%M:%S")
+
+        with open(srtfile,'a') as f:
+            f.write("{}\n".format(num+1))
+            f.write("{starttime} --> {endtime}\n".format(starttime=string_start,endtime=string_end))
+            f.write("{}\n".format(predlabels))
+            f.write("\n")
---
+++ run_generate_srt.py
@@ -0,0 +1,146 @@
+from torchvision.models.detection import fasterrcnn_resnet50_fpn
+from torchvision.models import densenet161
+import torch
+from torchvision.io import read_video,read_video_timestamps
+import torchvision.transforms as transforms
+import objectdetection
+import placesCNN_basic
+import torch.nn as nn
+from importlib import reload
+from tqdm import tqdm
+import os 
+import sys
+
+videofile = sys.argv[1]
+srtfile = (videofile[:-3] + 'srt')
+
+if os.path.isfile(srtfile):
+    os.remove(srtfile)
+
+normalize = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+
+# prepare the image transformer for Places Network
+centre_crop = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((256,256)),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        normalize
+])
+
+COCO_INSTANCE_CATEGORY_NAMES = [
+    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+] ### COCO Categories
+
+categories = objectdetection.categories ### ImageNet Categories
+
+places_categories= placesCNN_basic.classes ### Places Categories 
+
+### Define and register hook for extracting output feature map 
+#visualisation = []
+
+#def hook_fn(m, i, o):
+#    visualisation.append(o) 
+
+#model.roi_heads.box_predictor.cls_score.register_forward_hook(hook_fn)
+
+fps = 24
+nb_frames = 1
+
+nbsec = 3
+
+n_obj = 3
+
+beg_film = 1
+end_film = 600
+
+allpreds = []
+onsets = []
+
+model = fasterrcnn_resnet50_fpn(pretrained=True)
+model.eval()
+
+model_imagenet = densenet161(pretrained=True)
+model_imagenet.eval()
+
+model_places = placesCNN_basic.model.eval()
+
+
+n=0
+
+for curstart in tqdm(range(beg_film,end_film,nbsec)):
+
+    start = curstart
+    end = start + (nb_frames/fps)
+
+    vframes, aframes, info = read_video(filename=videofile,start_pts = start,end_pts=end,pts_unit='sec')
+
+    vframes = vframes.permute(0,3,1,2).float() / 255
+
+    _,H,C,V = vframes.shape
+
+
+    ### make prediction for Places 
+
+    im_norm = centre_crop(vframes[0]).reshape(1,3,224,224)
+    preds_places = model_places(im_norm)
+
+    ### make prediction for Imagenet classification 
+
+    im_norm = normalize(vframes[0]).reshape(1,H,C,V)
+    
+    preds_class= model_imagenet(im_norm)
+
+    ### make predictions for object detection
+
+    preds = model(vframes)
+
+    ### Associate Detection labels to prediction and keep only the first n_obj
+
+    predlabels_det = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in preds[0]['labels'].numpy()[:n_obj]]
+
+    ### Associate Classification labels to prediction 
+
+    allclasses = preds_class.data.numpy()[0]
+
+    # process output of Imagenet Classes and print results:
+    order = allclasses.argsort()
+    last = len(categories)-1
+    text = ''
+    for i in range(min(3, last+1)):
+        text += categories[order[last-i]]
+        text += ', '
+    text=text[:-2]
+
+
+    # process output of Places Classes and print results:
+
+    _, idx = preds_places[0].sort(0, True)
+
+    textplaces = ''
+    # output the prediction
+    for i in range(0, 5):
+        textplaces += places_categories[idx[i]]
+        textplaces += ', '
+    textplaces = textplaces[:-2]
+
+    ### Generate final string
+
+    annotation_str = "PLACES: {places} \nCOCO : {coco} \nImageNet : {net}".format(places=textplaces,coco=str(predlabels_det),net=text)
+
+    print(annotation_str)
+
+    ### Append to srt file with timecode 
+    objectdetection.gen_srt(annotation_str,start,srtfile=srtfile,num=n)
+    n=n+1

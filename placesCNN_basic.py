c2de7333c81b507362411eca8c240fecceee057a
Nicolas Farrugia <nicolas.farrugia@imt-atlantique.fr>
2020-02-15 23:48:35 +0100

    removed unused parts

--- placesCNN_basic.py
+++ placesCNN_basic.py
@@ -1,6 +1,6 @@
 # PlacesCNN for scene classification
 #
-# initially from https://github.com/CSAILVision/places365 , by Bolei Zhou,
+# based on https://github.com/CSAILVision/places365 , by Bolei Zhou,
 # modified by Nicolas Farrugia
 
 import torch
@@ -9,7 +9,7 @@ import torchvision.models as models
 from torchvision import transforms as trn
 from torch.nn import functional as F
 import os
-from PIL import Image
+
 
 # th architecture to use
 arch = 'resnet18'
@@ -26,15 +26,6 @@ state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict']
 model.load_state_dict(state_dict)
 model.eval()
 
-
-# load the image transformer
-centre_crop = trn.Compose([
-        trn.Resize((256,256)),
-        trn.CenterCrop(224),
-        trn.ToTensor(),
-        trn.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-])
-
 # load the class label
 file_name = 'categories_places365.txt'
 if not os.access(file_name, os.W_OK):
@@ -45,25 +36,3 @@ with open(file_name) as class_file:
     for line in class_file:
         classes.append(line.strip().split(' ')[0][3:])
 classes = tuple(classes)
-
-
-""" 
-# load the test image
-img_name = '12.jpg'
-if not os.access(img_name, os.W_OK):
-    img_url = 'http://places.csail.mit.edu/demo/' + img_name
-    os.system('wget ' + img_url)
-
-img = Image.open(img_name)
-input_img = V(centre_crop(img).unsqueeze(0))
-
-# forward pass
-logit = model.forward(input_img)
-h_x = F.softmax(logit, 1).data.squeeze()
-probs, idx = h_x.sort(0, True)
-
-print('{} prediction on {}'.format(arch,img_name))
-# output the prediction
-for i in range(0, 5):
-    print('{:.3f} -> {}'.format(probs[i], classes[idx[i]]))
- """

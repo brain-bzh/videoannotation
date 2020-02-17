c863c7a5313251acd6f0cf8dac81f20ded733e1e
Nicolas Farrugia <nicolas.farrugia@imt-atlantique.fr>
2020-02-17 17:42:30 +0100

    renamed objectdetection to utils

--- utils.py
+++ utils.py
@@ -9,21 +9,18 @@ from matplotlib import pyplot as plt
 
 import datetime
 
+import os
 
-COCO_INSTANCE_CATEGORY_NAMES = [
-    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
-    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
-    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
-    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
-    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
-    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
-    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
-    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
-    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
-    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
-]
+
+
+def convert_Audio(mediaFile, outPath, outExtension = '.wav', name = None):
+    if name == None : 
+        name, extension = os.path.splitext(os.path.basename(mediaFile))
+    outFile = os.path.join(outPath, name+outExtension)
+    cmd = 'ffmpeg -i '+mediaFile+' '+outFile
+    os.system(cmd)
+    return outFile
+
 
 #### imagenet categories
 

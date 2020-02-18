7d75b401f4719f682cc5ffbaf002c8d1faf58576
Nicolas Farrugia <nicolas.farrugia@imt-atlantique.fr>
2020-02-18 15:43:00 +0100

    fixed a remove

--- utils.py
+++ utils.py
@@ -40,17 +40,30 @@ def cat_file():
 
 categories = cat_file() # load category file
 
+COCO_INSTANCE_CATEGORY_NAMES = [
+    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+]
 
 
 
 def annotate_img(preds,vframes,n_obj=5):
-
+    global COCO_INSTANCE_CATEGORY_NAMES
     ### vframes  : last three dims input tensor to faster_rcnn
 
     ### preds : dictionary of outputs of fater_rcnn
 
-    global COCO_INSTANCE_CATEGORY_NAMES
-
+    
     predlabels = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in preds['labels'].numpy()]
     scores = [i for i in preds['scores'].detach().numpy()]
     bboxes = [i for i in preds['boxes'].detach().numpy()]

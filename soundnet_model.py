615ac28f99cf3864026d0e3f2ebcbf90b78358c1
Nicolas Farrugia <nicolas.farrugia@imt-atlantique.fr>
2020-02-19 12:07:56 +0100

    added small model

--- soundnet_model.py
+++ soundnet_model.py
@@ -1,6 +1,78 @@
 import torch
 import torch.nn as nn
 
+
+
+class SmallestWaveCNN(nn.Module):
+    def __init__(self):
+        super(SmallestWaveCNN, self).__init__()
+        
+        self.define_module()
+        
+    def define_module(self):
+
+        #The hyperparameters of this network have been set for 12 kHz, 1 second long waveforms. 
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1,4, (64,1), (2,1), (32,0), bias=True),
+            nn.BatchNorm2d(4),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d((2,1), (2,1))
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(4, 4, (32,1), (2,1), (16,0), bias=True),
+            nn.BatchNorm2d(4),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d((2,1),(2,1))
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(4, 4, (16,1), (2,1), (8,0), bias=True),
+            nn.BatchNorm2d(4),
+            nn.ReLU(inplace=True)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(4, 4, (8,1), (2,1), (4,0), bias=True),
+            nn.BatchNorm2d(4),
+            nn.ReLU(inplace=True),
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(4, 4, (4,1),(2,1),(2,0), bias=True),
+            nn.BatchNorm2d(4),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d((4,1),(4,1))
+        ) 
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(4, 4, (4,1), (2,1), (2,0), bias=True),
+            nn.BatchNorm2d(4),
+            nn.ReLU(inplace=True)
+        )
+        self.conv7 = nn.Sequential(
+            nn.Conv2d(4, 4, (4,1), (2,1), (2,0), bias=True),
+            nn.BatchNorm2d(4),
+            nn.ReLU(inplace=True)
+        )
+        self.object_emb = nn.Sequential(
+            nn.Conv2d(4, 1000, (7,1), bias=True),
+        ) 
+        self.scene_emb = nn.Sequential(
+            nn.Conv2d(4, 365, (7,1), bias=True)
+        )
+        self.audiotag_emb = nn.Sequential(
+            nn.Conv2d(4, 527, (7,1), bias=True)
+        )
+
+
+    def forward(self, x):
+        for net in [self.conv1, self.conv2, self.conv3, self.conv4, self.conv5, self.conv6, self.conv7]:
+            x = net(x)
+        object_emb = self.object_emb(x)
+        scene_emb = self.scene_emb(x) 
+        audiotag_emb = self.audiotag_emb(x)
+        return object_emb, scene_emb, audiotag_emb
+
+
+
+
 class SmallerWaveCNN(nn.Module):
     def __init__(self):
         super(SmallerWaveCNN, self).__init__()

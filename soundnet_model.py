96abd9a5d4fbf194ba533c052d6ec998bbf451f8
Nicolas Farrugia <nicolas.farrugia@imt-atlantique.fr>
2020-02-19 09:35:11 +0100

    added SmallerWaveCNN

--- soundnet_model.py
+++ soundnet_model.py
@@ -1,24 +1,27 @@
 import torch
 import torch.nn as nn
 
-class SoundNet8_pytorch(nn.Module):
+class SmallerWaveCNN(nn.Module):
     def __init__(self):
-        super(SoundNet8_pytorch, self).__init__()
+        super(SmallerWaveCNN, self).__init__()
         
         self.define_module()
         
     def define_module(self):
+
+        #The hyperparameters of this network have been set for 12 kHz, 1 second long waveforms. 
+
         self.conv1 = nn.Sequential(
             nn.Conv2d(1,16, (64,1), (2,1), (32,0), bias=True),
             nn.BatchNorm2d(16),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d((8,1), (8,1))
+            nn.MaxPool2d((2,1), (2,1))
         )
         self.conv2 = nn.Sequential(
             nn.Conv2d(16, 32, (32,1), (2,1), (16,0), bias=True),
             nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d((8,1),(8,1))
+            nn.MaxPool2d((2,1),(2,1))
         )
         self.conv3 = nn.Sequential(
             nn.Conv2d(32, 64, (16,1), (2,1), (8,0), bias=True),
@@ -35,7 +38,7 @@ class SoundNet8_pytorch(nn.Module):
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             nn.MaxPool2d((4,1),(4,1))
-        ) # difference here (0.24751323, 0.2474), padding error has beed debuged
+        ) 
         self.conv6 = nn.Sequential(
             nn.Conv2d(256, 512, (4,1), (2,1), (2,0), bias=True),
             nn.BatchNorm2d(512),
@@ -44,6 +47,73 @@ class SoundNet8_pytorch(nn.Module):
         self.conv7 = nn.Sequential(
             nn.Conv2d(512, 1024, (4,1), (2,1), (2,0), bias=True),
             nn.BatchNorm2d(1024),
+            nn.ReLU(inplace=True)
+        )
+        self.object_emb = nn.Sequential(
+            nn.Conv2d(1024, 512, (7,1), bias=True),
+        ) 
+        self.scene_emb = nn.Sequential(
+            nn.Conv2d(1024, 512, (7,1), bias=True)
+        )
+        self.audiotag_emb = nn.Sequential(
+            nn.Conv2d(1024, 512, (7,1), bias=True)
+        )
+
+
+    def forward(self, x):
+        for net in [self.conv1, self.conv2, self.conv3, self.conv4, self.conv5, self.conv6, self.conv7]:
+            x = net(x)
+        object_emb = self.object_emb(x)
+        scene_emb = self.scene_emb(x) 
+        audiotag_emb = self.audiotag_emb(x)
+        return object_emb, scene_emb, audiotag_emb
+
+
+
+
+class SoundNet8_pytorch(nn.Module):
+    def __init__(self):
+        super(SoundNet8_pytorch, self).__init__()
+        
+        self.define_module()
+        
+    def define_module(self):
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1,16, (64,1), (2,1), (32,0), bias=True),
+            #nn.BatchNorm2d(16),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d((8,1), (8,1))
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, (32,1), (2,1), (16,0), bias=True),
+            #nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d((8,1),(8,1))
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(32, 64, (16,1), (2,1), (8,0), bias=True),
+            #nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(64, 128, (8,1), (2,1), (4,0), bias=True),
+            #nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(128, 256, (4,1),(2,1),(2,0), bias=True),
+            #nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d((4,1),(4,1))
+        ) # difference here (0.24751323, 0.2474), padding error has beed debuged
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(256, 512, (4,1), (2,1), (2,0), bias=True),
+            #nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True)
+        )
+        self.conv7 = nn.Sequential(
+            nn.Conv2d(512, 1024, (4,1), (2,1), (2,0), bias=True),
+            #nn.BatchNorm2d(1024),
             nn.ReLU(inplace=True)
         )
         self.object_emb = nn.Sequential(

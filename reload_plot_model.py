1dc2c53693a153311356bcbea3392604e3fd18be
Nicolas Farrugia <nicolas.farrugia@imt-atlantique.fr>
2020-02-23 15:46:25 +0100

    added configurable plotting

--- reload_plot_model.py
+++ reload_plot_model.py
@@ -24,11 +24,14 @@ from sklearn.metrics import r2_score
 from nilearn.plotting import plot_stat_map
 
 from nilearn.regions import signals_to_img_labels
+import sys
 
+modelfile = sys.argv[1]
 
+modeldict = torch.load(modelfile)
 
-ninputfilters = 2
-nfeat = 1
+#ninputfilters = modelfile['ninputfilters']
+#nfeat = modelfile['nfeat']
 
 testloader = DataLoader(testset,batch_size=512)
 
@@ -60,7 +63,8 @@ def test_kl_r2(testloader,net,kl_im,kl_audio,kl_places,mseloss):
     return r2_model
 
 ### Model Setup
-net = WaveformCNN(nfeat=nfeat,ninputfilters=ninputfilters,do_encoding_fmri=True)
+#net = WaveformCNN(nfeat=nfeat,ninputfilters=ninputfilters,do_encoding_fmri=True)
+net = modeldict['model']
 net = net.cuda()
 
 kl_im = nn.KLDivLoss(reduction='batchmean')
@@ -68,17 +72,32 @@ kl_audio = nn.KLDivLoss(reduction='batchmean')
 kl_places = nn.KLDivLoss(reduction='batchmean')
 mseloss = nn.MSELoss(reduction='mean')
 
-checkpoint_path = 'checkpoint.pt'
+checkpoint_path = 'bestmodel.pt'
 ## Reload best model 
-net.load_state_dict(torch.load(checkpoint_path))
+net.load_state_dict(modeldict['net'])
+
 
 r2model = test_kl_r2(testloader,net,kl_im,kl_audio,kl_places,mseloss=mseloss)
+r2model[r2model<0] = 0
 print("mean R2 score on test set  : {}".format(r2model.mean()))
 
 print("max R2 score on test set  : {}".format(r2model.max()))
 
+### Plot the loss figure
+f = plt.figure()
+
+ax = plt.subplot(2,1,2)
+
+plt.plot(modeldict['train_loss'])
+plt.plot(modeldict['val_loss'])
+plt.legend(['Train','Test'])
+plt.title("Mean $R^2=${}, Max $R^2=${}".format(r2model.mean(),r2model.max()))
+
+### R2 figure 
 r2_img = signals_to_img_labels(r2model.reshape(1,-1),'/home/nfarrugi/git/MIST_parcellation/MIST_parcellation/Parcellations/MIST_ROI.nii.gz')
 
-plot_stat_map(r2_img,display_mode='z',cut_coords=8)
+ax = plt.subplot(2,1,1)
+
+plot_stat_map(r2_img,display_mode='z',cut_coords=8,figure=f,axes=ax)
 plt.show()
 r2_img.to_filename('r2_img.nii.gz')

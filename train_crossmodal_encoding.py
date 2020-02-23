89629ead5ace9b98b6a5fe72ed60897d69976d14
Nicolas Farrugia <nicolas.farrugia@imt-atlantique.fr>
2020-02-23 15:59:30 +0100

    added plot generation, added loop on hyperparam loss

--- train_crossmodal_encoding.py
+++ train_crossmodal_encoding.py
@@ -19,108 +19,143 @@ from train_utils import train_kl,test_kl,AudioToEmbeddings,trainloader,valloader
 from pytorchtools import EarlyStopping
 from datetime import datetime
 
-nfeat = 2
-ninputfilters = 8
+from nilearn.plotting import plot_stat_map
+
+from nilearn.regions import signals_to_img_labels
+
+mistroifile = '/home/brain/MIST_ROI.nii.gz'
+
+alpha = 0
+beta = 1
+gamma = 1
+delta = 1
+
+destdir = 'cp_{}_{}_{}_{}'.format(alpha,beta,gamma,delta)
+
+for alpha in [1e-6,1]:
+    for beta in [1,1e-6]:
+        for gamma in [1,1e-6]:
+            for delta in [1,1e-6]:
+        
+                for ninputfilters in [1,2,4]:
+                    for nfeat in [ninputfilters,2*ninputfilters]:
+
+                        ### Model Setup
+                        net = WaveformCNN(nfeat=nfeat,ninputfilters=ninputfilters,do_encoding_fmri=True)
+                        net = net.cuda()
+                        kl_im = nn.KLDivLoss(reduction='batchmean')
+                        kl_audio = nn.KLDivLoss(reduction='batchmean')
+                        kl_places = nn.KLDivLoss(reduction='batchmean')
+                        mseloss = nn.MSELoss(reduction='mean')
+
+                        ### Optimizer and Schedulers
+                        optimizer = torch.optim.SGD(net.parameters(),lr=0.001)
+                        lr_sched = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,factor=0.2,patience=4,threshold=1e-4)
+                        #optimizer = torch.optim.Adam(net.parameters())
+
+                        # initialize the early_stopping object
+                        early_stopping = EarlyStopping(patience=8, verbose=True)
+                        nbepoch = 5000
+
+                        #### Simple test just to check the shapes
+
+                        if True:
+                            from train_utils import dataset
+
+                            with torch.no_grad():
+                                
+                                onesample = dataset.__getitem__(25)
+
+                                bsize = 1
+
+                                # load data
+                                wav = torch.Tensor(onesample['waveform']).view(bsize,1,-1,1).cuda()
+                                places = torch.Tensor(onesample['places']).view(bsize,-1,1,1).cuda()
+                                audioset = torch.Tensor(onesample['audioset']).view(bsize,-1,1,1).cuda()
+                                imnet = torch.Tensor(onesample['imagenet']).view(bsize,-1,1,1).cuda()
+                                fmri = torch.Tensor(onesample['fmri']).view(bsize,-1).cuda()
+
+                                print(wav.shape)
+
+                                print(imnet.shape,places.shape,audioset.shape,fmri.shape)
+                                obj_p,scene_p,audio_p,fmri_p = net(wav)
+                                print(obj_p.shape,scene_p.shape,audio_p.shape,fmri_p.shape)
+
+                        ### Main Training Loop 
+                        startdate = datetime.now()
+
+                        train_loss = []
+                        val_loss = []
+                        for epoch in tqdm(range(nbepoch)):
+                            train_loss.append(train_kl(epoch,trainloader,net,optimizer,kl_im,kl_audio,kl_places,mseloss=mseloss,alpha=alpha,beta=beta,gamma=gamma,delta=delta))
+                            val_loss.append(test_kl(epoch,valloader,net,optimizer,kl_im,kl_audio,kl_places,mseloss=mseloss,alpha=alpha,beta=beta,gamma=gamma,delta=delta))
+                            #print("Train : {}, Val : {} ".format(train_loss[-1],val_loss[-1]))
+                            lr_sched.step(val_loss[-1])
+
+                            # early_stopping needs the validation loss to check if it has decresed, 
+                                # and if it has, it will make a checkpoint of the current model
+                            early_stopping(val_loss[-1], net)
+                            
+                            if early_stopping.early_stop:
+                                print("Early stopping")
+                                break
+
+                        test_loss = test_kl(1,testloader,net,optimizer,kl_im,kl_audio,kl_places,mseloss=mseloss,alpha=alpha,beta=beta,gamma=gamma,delta=delta)
+                        print("Test Loss : {}".format(test_loss))
+
+                        enddate = datetime.now()
+
+                        ## Reload best model 
+                        net.load_state_dict(torch.load('checkpoint.pt'))
+
+                        ## Prepare data structure for checkpoint
+                        state = {
+                                    'net': net.state_dict(),
+                                    'epoch': epoch,
+                                    'train_loss' : train_loss,
+                                    'val_loss' : val_loss,
+                                    'test_loss' : test_loss,
+                                    'nfeat' : nfeat,
+                                    'ninputfilters' : ninputfilters,
+                                    'model' : net
+                                }
+
+                        if not os.path.isdir(destdir):
+                            os.mkdir(destdir)
+
+                        dt_string = enddate.strftime("%Y-%m-%d-%H-%M-%S")
+                        str_bestmodel = os.path.join(destdir,"{}.pt".format(dt_string))
+                        str_bestmodel_plot = os.path.join(destdir,"{}_{}_{}.png".format(dt_string,ninputfilters,nfeat))
+                        str_bestmodel_nii = os.path.join(destdir,"{}_{}_{}.nii.gz".format(dt_string,ninputfilters,nfeat))
 
 
-for ninputfilters in [16,2,4]:
-    for nfeat in [int(ninputfilters/2),ninputfilters,2*ninputfilters,4*ninputfilters]:
+                        torch.save(state, str_bestmodel)
 
-        ### Model Setup
-        net = WaveformCNN(nfeat=nfeat,ninputfilters=ninputfilters,do_encoding_fmri=True)
-        net = net.cuda()
-        kl_im = nn.KLDivLoss(reduction='batchmean')
-        kl_audio = nn.KLDivLoss(reduction='batchmean')
-        kl_places = nn.KLDivLoss(reduction='batchmean')
-        mseloss = nn.MSELoss(reduction='mean')
+                        # Remove temp file 
+                        os.remove('checkpoint.pt')
 
-        ### Optimizer and Schedulers
-        optimizer = torch.optim.SGD(net.parameters(),lr=0.001)
-        lr_sched = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,factor=0.2,patience=4,threshold=1e-4)
-        #optimizer = torch.optim.Adam(net.parameters())
+                        r2model = test_kl_r2(testloader,net,kl_im,kl_audio,kl_places,mseloss=mseloss)
+                        r2model[r2model<0] = 0
+                        print("mean R2 score on test set  : {}".format(r2model.mean()))
 
-        # initialize the early_stopping object
-        early_stopping = EarlyStopping(patience=8, verbose=True)
-        nbepoch = 5000
+                        print("max R2 score on test set  : {}".format(r2model.max()))
 
-        #### Simple test just to check the shapes
+                        ### Plot the loss figure
+                        f = plt.figure(figsize=(20,10))
 
-        if True:
-            from train_utils import dataset
+                        ax = plt.subplot(2,1,2)
 
-            with torch.no_grad():
-                
-                onesample = dataset.__getitem__(25)
+                        plt.plot(state['train_loss'])
+                        plt.plot(state['val_loss'])
+                        plt.legend(['Train','Test'])
+                        plt.title("Mean $R^2=${}, Max $R^2=${}".format(r2model.mean(),r2model.max()))
 
-                bsize = 1
+                        ### R2 figure 
+                        r2_img = signals_to_img_labels(r2model.reshape(1,-1),mistroifile)
 
-                # load data
-                wav = torch.Tensor(onesample['waveform']).view(bsize,1,-1,1).cuda()
-                places = torch.Tensor(onesample['places']).view(bsize,-1,1,1).cuda()
-                audioset = torch.Tensor(onesample['audioset']).view(bsize,-1,1,1).cuda()
-                imnet = torch.Tensor(onesample['imagenet']).view(bsize,-1,1,1).cuda()
-                fmri = torch.Tensor(onesample['fmri']).view(bsize,-1).cuda()
+                        ax = plt.subplot(2,1,1)
 
-                print(wav.shape)
-
-                print(imnet.shape,places.shape,audioset.shape,fmri.shape)
-                obj_p,scene_p,audio_p,fmri_p = net(wav)
-                print(obj_p.shape,scene_p.shape,audio_p.shape,fmri_p.shape)
-
-        ### Main Training Loop 
-        startdate = datetime.now()
-
-        train_loss = []
-        val_loss = []
-        for epoch in tqdm(range(nbepoch)):
-            train_loss.append(train_kl(epoch,trainloader,net,optimizer,kl_im,kl_audio,kl_places,mseloss=mseloss))
-            val_loss.append(test_kl(epoch,valloader,net,optimizer,kl_im,kl_audio,kl_places,mseloss=mseloss))
-            #print("Train : {}, Val : {} ".format(train_loss[-1],val_loss[-1]))
-            lr_sched.step(val_loss[-1])
-
-            # early_stopping needs the validation loss to check if it has decresed, 
-                # and if it has, it will make a checkpoint of the current model
-            early_stopping(val_loss[-1], net)
-            
-            if early_stopping.early_stop:
-                print("Early stopping")
-                break
-
-        test_loss = test_kl(1,testloader,net,optimizer,kl_im,kl_audio,kl_places,mseloss=mseloss)
-        print("Test Loss : {}".format(test_loss))
-
-        enddate = datetime.now()
-
-        ## Reload best model 
-        net.load_state_dict(torch.load('checkpoint.pt'))
-
-        ## Prepare data structure for checkpoint
-        state = {
-                    'net': net.state_dict(),
-                    'epoch': epoch,
-                    'train_loss' : train_loss,
-                    'val_loss' : val_loss,
-                    'test_loss' : test_loss,
-                    'nfeat' : nfeat,
-                    'ninputfilters' : ninputfilters,
-                    'model' : net
-                }
-
-        if not os.path.isdir('checkpoints_encoding'):
-            os.mkdir('checkpoints_encoding')
-
-        dt_string = enddate.strftime("%Y-%m-%d-%H-%M-%S")
-        str_bestmodel = os.path.join('checkpoints_encoding',"{}.pt".format(dt_string))
-        str_bestmodel_plot = os.path.join('checkpoints_encoding',"{}_{}_{}.png".format(dt_string,ninputfilters,nfeat))
-
-        torch.save(state, str_bestmodel)
-
-        # Remove temp file 
-        os.remove('checkpoint.pt')
-
-        ## Plot losses 
-        plt.plot(train_loss)
-        plt.plot(val_loss)
-        plt.ylim([7,11])
-        plt.savefig(str_bestmodel_plot)
-        plt.close()
+                        plot_stat_map(r2_img,display_mode='z',cut_coords=8,figure=f,axes=ax)
+                        f.savefig(str_bestmodel_plot)
+                        r2_img.to_filename(str_bestmodel_nii)
+                        plt.close()

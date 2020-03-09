9be633aa0500311720d8f85cb7fc67d5f84d9135
Nicolas Farrugia <nicolas.farrugia@imt-atlantique.fr>
2020-03-09 13:42:58 +0100

    added original soundnet model

--- soundnet_model.py
+++ soundnet_model.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 class WaveformCNN8(nn.Module):
     def __init__(self,nfeat=16,ninputfilters=16,do_encoding_fmri=False,nroi=210,fmrihidden=1000,nroi_attention=None):
         super(WaveformCNN8, self).__init__()
-
+        raise("DEPRECATED")
         ## Hyper parameters of the main branch
         self.nfeat = nfeat
         self.ninputfilters = 16
@@ -183,3 +183,80 @@ class WaveformCNN5(nn.Module):
         return object_emb, scene_emb, audiotag_emb,x
 
 
+class SoundNet8_pytorch(nn.Module):
+    def __init__(self):
+        super(SoundNet8_pytorch, self).__init__()
+        
+        self.define_module()
+        
+    def define_module(self):
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1,16, (64,1), (2,1), (32,0), bias=True),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d((8,1), (8,1))
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, (32,1), (2,1), (16,0), bias=True),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d((8,1),(8,1))
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(32, 64, (16,1), (2,1), (8,0), bias=True),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(64, 128, (8,1), (2,1), (4,0), bias=True),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(128, 256, (4,1),(2,1),(2,0), bias=True),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d((4,1),(4,1))
+        ) # difference here (0.24751323, 0.2474), padding error has beed debuged
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(256, 512, (4,1), (2,1), (2,0), bias=True),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True)
+        )
+        self.conv7 = nn.Sequential(
+            nn.Conv2d(512, 1024, (4,1), (2,1), (2,0), bias=True),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(inplace=True)
+        )
+        self.conv8 = nn.Sequential(
+            nn.Conv2d(1024, 1000, (8,1), (2,1), (0,0), bias=True),
+        ) 
+        self.conv8_2 = nn.Sequential(
+            nn.Conv2d(1024, 401, (8,1), (2,1), (0,0), bias=True)
+        )
+
+    def forward(self, x):
+        for net in [self.conv1, self.conv2, self.conv3, self.conv4, self.conv5, self.conv6, self.conv7]:
+            x = net(x)
+        object_pred = self.conv8(x)
+        scene_pred = self.conv8_2(x) 
+        return x,object_pred, scene_pred
+
+    def extract_feat(self,x:torch.Tensor)->list:
+        output_list = []
+        for net in [self.conv1, self.conv2, self.conv3, self.conv4, self.conv5, self.conv6, self.conv7]:
+            x = net(x)
+            output_list.append(x.detach().cpu().numpy())
+        object_pred = self.conv8(x)
+        output_list.append(object_pred.detach().cpu().numpy())
+        scene_pred = self.conv8_2(x) 
+        output_list.append(scene_pred.detach().cpu().numpy())
+        return output_list
+    
+    
+    def extract_feat_nooutput(self,x:torch.Tensor)->list:
+        output_list = []
+        for net in [self.conv1, self.conv2, self.conv3, self.conv4, self.conv5, self.conv6, self.conv7]:
+            x = net(x)
+            output_list.append(x.detach().cpu().numpy())        
+        return output_list
